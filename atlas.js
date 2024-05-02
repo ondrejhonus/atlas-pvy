bb31894417f784be66bc4cfff32319308273cb77
ondrejhonus <honusondrej@gmail.com>
2024-05-02 09:27:22 +0200

    init

---
+++ atlas.js
@@ -0,0 +1,9 @@
+fetch('https://restcountries.com/v3.1/region/europe')
+    .then(response => response.json())
+    .then(data => {
+
+        console.log(data[0].flags.png)
+        const img = document.createElement('img');
+        img.src = data[0].flags.png;
+        document.body.appendChild(img);
+    })
