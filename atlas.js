eb36e2729f4630e947634b25acc887649f18d2c1
ondrejhonus <honusondrej@gmail.com>
2024-05-04 08:33:09 +0200

    03 - all countries

--- atlas.js
+++ atlas.js
@@ -1,7 +1,20 @@
 fetch('https://restcountries.com/v3.1/region/europe')
     .then(response => response.json())
     .then(data => {
-
-        console.log(data[0].flags.png)
-        const img = document.createElement('img');
-    })
+        data.forEach(country => {
+            console.log(country.capital[0]);
+            let stat = `<div class="col-xl-2 col-lg-3 col-md-4 col-sm-6">
+            <div class="card">
+                <a href="${country.maps.googleMaps}" target="_blank">
+                <img class="card-img-top img-flag" src="${country.flags.svg}" alt="${country.flags.alt}"/>
+                </a>
+                <div class="card-body">
+                    <h4 class="card-title">${country.name.common}</h4>
+                    <p class="card-text">Population: ${country.population}</p>
+                    <p>Capital : ${country.capital[0]}</p>
+                </div>
+            </div>
+        </div>`;
+        document.getElementById('staty').innerHTML += stat;
+        });
+});
