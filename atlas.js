7ebd0ee3756c355523ce1e23a7e6039804e6e098
ondrejhonus <honusondrej@gmail.com>
2024-05-14 17:01:34 +0200

    04 - search bar

--- atlas.js
+++ atlas.js
@@ -1,20 +1,39 @@
+// Fetch country data and render initial list
+let countriesData = [];
+
 fetch('https://restcountries.com/v3.1/region/europe')
     .then(response => response.json())
     .then(data => {
-        data.forEach(country => {
-            console.log(country.capital[0]);
-            let stat = `<div class="col-xl-2 col-lg-3 col-md-4 col-sm-6">
-            <div class="card">
-                <a href="${country.maps.googleMaps}" target="_blank">
-                <img class="card-img-top img-flag" src="${country.flags.svg}" alt="${country.flags.alt}"/>
-                </a>
-                <div class="card-body">
-                    <h4 class="card-title">${country.name.common}</h4>
-                    <p class="card-text">Population: ${country.population}</p>
-                    <p>Capital : ${country.capital[0]}</p>
-                </div>
+        countriesData = data;
+        renderCountries(data);
+    });
+
+function renderCountries(data) {
+    const statesElement = document.getElementById('states');
+    statesElement.innerHTML = '';
+
+    data.forEach(country => {
+        let stat = `<div class="col-xl-2 col-lg-3 col-md-4 col-sm-6">
+        <div class="card">
+            <a href="${country.maps.googleMaps}" target="_blank">
+            <img class="card-img-top img-flag" src="${country.flags.svg}" alt="${country.flags.alt}"/>
+            </a>
+            <div class="card-body">
+                <h5 class="card-title strongaa">${country.name.common}</h4>
+                <p class="card-text">Population: ${country.population}</p>
+                <p>Capital : ${country.capital ? country.capital[0] : 'N/A'}</p>
             </div>
-        </div>`;
-        document.getElementById('staty').innerHTML += stat;
-        });
+        </div>
+    </div>`;
+        statesElement.innerHTML += stat;
+    });
+}
+
+// Add event listener to search input
+document.getElementById('search').addEventListener('input', function (e) {
+    const query = e.target.value.toLowerCase();
+    const filteredCountries = countriesData.filter(country =>
+        country.name.common.toLowerCase().includes(query)
+    );
+    renderCountries(filteredCountries);
 });
